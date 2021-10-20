c1678a3062461a4ada9129c91c3c5a728db2f44e
paholt53 <92401363+paholt53@users.noreply.github.com>
2021-10-20 18:30:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    line1 = "If you deposit ";
+    line2 = ",\<br\> at an interest rate of ";
+    lime3 = "%\<br\> You will receive an amount of ";
+    line4 = ",\<br\> in the year ";
+    endline = "\<br\>";
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    interest = principal * years * rate / 100;
+    year =  new Date().getFullYear() + parseInt(years);
+    
+    If(number(years)) // if years is numeric
+    {  If (years > 1900 && years < 2500) 
+     {  document.getElementById("result").innerHTML=line1+principal+limne2+rate+line3+interest+line4+year+endline;
+     }
+    }
+}    
+function updateRate()
+{
+    rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+            
     
 }
         
